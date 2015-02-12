81ef5732b8dc69b2dbe0834093ef744b3d5fd9ac
jLepjaks <j.lepjaks@my.westminster.ac.uk>
2015-02-12 17:45:38 +0000

    replaced few lines of code
    now can separetly work on user details

--- GroupMobleApplication/src/groupmobleapplication/SQLconnection.java
+++ GroupMobleApplication/src/groupmobleapplication/SQLconnection.java
@@ -9,8 +9,7 @@ public class SQLconnection {
 
     public static void main(String[] argv) {
         System.out.println("-------- Simple MySQL Application ------------");
-        
-        
+
         Tunnel t = new Tunnel();
         try {
             t.go();
@@ -33,12 +32,11 @@ public class SQLconnection {
         try // open the connection to the MySQL server
         {
             connection = DriverManager.getConnection(
-                  // "jdbc:mysql://localhost:8889/w1439058_0", "root", "root");
-            
-               //"jdbc:mysql://elephant.ecs.westminster.ac.uk:3306/w1439058_0",     "w1439058", "Sz2eZkchSIXI");
-            
-"jdbc:mysql://localhost:9080/w1439058_0",     "w1439058", "Sz2eZkchSIXI");
-                    
+                    "jdbc:mysql://localhost:9080/w1439058_0", "w1439058", "Sz2eZkchSIXI");
+//this connetion realy works, if needed change ports 9080 to any avaliable hsould mach "tunnelLocalPort" from Tunnel.java.
+
+            // "jdbc:mysql://localhost:8889/w1439058_0", "root", "root");
+            //"jdbc:mysql://elephant.ecs.westminster.ac.uk:3306/w1439058_0",     "w1439058", "Sz2eZkchSIXI");
         } catch (SQLException e) {
             System.out.println("ERROR: MySQL Connection Failed!");
             e.printStackTrace();
@@ -60,18 +58,25 @@ public class SQLconnection {
         try // try to run the query
         {
             queryRes = stmt.executeQuery("select * from users");
-            int n = 0;    // a counter for the output
+            // int n = 0;    // a counter for the output
 
             while (queryRes.next()) // while there's still some more results of the query...
             {
-                int numColumns = queryRes.getMetaData().getColumnCount();   // get how many cols in this entry
-                n++;
-               // System.out.print("" + n);
-                for (int i = 1; i <= numColumns; i++) // loop through the cols to print them
-                {                                               // Column numbers start at 1.
-                    System.out.print("  " + queryRes.getObject(i));
-                }
-                System.out.println("");   // print a new line at the end of the entry.
+                String id = queryRes.getString("userId");
+                String firstName = queryRes.getString("userName");
+                String lastName = queryRes.getString("userPassword");
+
+                System.out.println("ID: " + id + ", USER name: " + firstName
+                        + ", Password: " + lastName);
+
+//                int numColumns = queryRes.getMetaData().getColumnCount();   // get how many cols in this entry
+//               // n++;
+//                // System.out.print("" + n);
+//                for (int i = 1; i <= numColumns; i++) // loop through the cols to print them
+//                {                                               // Column numbers start at 1.
+//                    System.out.print("  " + queryRes.getObject(i));
+//                }
+//                System.out.println("");   // print a new line at the end of the entry.
             }
         } catch (SQLException e) {
             System.out.println("ERROR: Cannot execute query.");
