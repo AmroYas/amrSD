6608311ae7617b179b3b72166525b80293a802ed
jLepjaks <j.lepjaks@my.westminster.ac.uk>
2015-02-12 17:25:56 +0000

    ssh and sql
    sshc connection and sql queries

---
+++ GroupMobleApplication/src/groupmobleapplication/Tunnel.java
@@ -0,0 +1,56 @@
+
+import com.jcraft.jsch.*;
+
+public class Tunnel {
+
+ 
+
+    public void go() throws Exception {
+        String host = "jaguar.wmin.ac.uk";
+        String user =  "w1439058";
+         String password = "zumarik101";
+        int port = 22;
+
+        int tunnelLocalPort = 9080;
+        String tunnelRemoteHost = "elephant.ecs.westminster.ac.uk";
+        int tunnelRemotePort = 3306;
+
+        JSch jsch = new JSch();
+        Session session = jsch.getSession(user, host, port);
+        session.setPassword(password);
+        localUserInfo lui = new localUserInfo();
+        session.setUserInfo(lui);
+        session.connect();
+        session.setPortForwardingL(tunnelLocalPort, tunnelRemoteHost, tunnelRemotePort);
+        System.out.println("Connected");
+
+    }
+
+    class localUserInfo implements UserInfo {
+
+        String passwd;
+
+        public String getPassword() {
+            return passwd;
+        }
+
+        public boolean promptYesNo(String str) {
+            return true;
+        }
+
+        public String getPassphrase() {
+            return null;
+        }
+
+        public boolean promptPassphrase(String message) {
+            return true;
+        }
+
+        public boolean promptPassword(String message) {
+            return true;
+        }
+
+        public void showMessage(String message) {
+        }
+    }
+}
